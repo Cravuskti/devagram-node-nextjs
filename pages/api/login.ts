73de9c622fb346cdc03d62df90a66e33561a3322
Cravuskti <cravuskti@gmail.com>
2022-03-05 20:18:04 -0300

    criado a paina de cadastro e corrido o login/refeito parametros no postman

--- pages/api/login.ts
+++ pages/api/login.ts
@@ -1,8 +1,10 @@
 import type { NextApiRequest, NextApiResponse } from 'next';
 import {conectarMongoDB} from'../../middlewares/conectarMongoDB';
 import type {respostaPadraoMsg} from '../../types/respostaPadraoMsg';
+import md5 from 'md5';
+import { usuarioModel } from '../../models/usuarioModel';
 
-const endpointLogin = (
+const endpointLogin = async (
     req : NextApiRequest, 
     res : NextApiResponse<respostaPadraoMsg>
 ) => {  
@@ -10,9 +12,10 @@ const endpointLogin = (
         
         const {login, senha} = req.body;
 
-        if(login === 'admin@admin.com' && 
-            senha === 'admin123'){
-            return res.status(200).json({msg : 'Usuario autenticado com sucesso!'})
+        const usuariosEncontrados = await usuarioModel.find({email : login, senha : md5(senha)});
+        if(usuariosEncontrados && usuariosEncontrados.length > 0){
+            const usuarioEncontrado = usuariosEncontrados[0]; 
+            return res.status(200).json({msg : `Usuario ${usuarioEncontrado.nome} autenticado com sucesso!`});
         }
         return res.status(400).json({erro : 'usuario ou senha nao encontrado'})
     }
